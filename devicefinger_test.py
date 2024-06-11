1230dc33324209a20980f3aac007df47559ef4a3
Arike Granger <945183225@qq.com>
2024-06-11 22:02:37 +0800

    feat: devicefinger_test.py
    新增伪造设备检出率

--- devicefinger_test.py
+++ devicefinger_test.py
@@ -16,7 +16,7 @@ def generate_car_data(num_samples, input_dim, num_negative, route_number):
     labels = np.zeros((num_samples, 5), dtype=int)
     waiting_time = 6  # 待料状态下需要等待6秒
     device_list = []
-    per_control = 0.5  # 控制数量占全部负样本的比例
+    per_control = 0.1  # 控制数量占全部负样本的比例
     print("开始生成样本")
     
     # 路线起始点和终点
@@ -158,11 +158,8 @@ def main():
     train_y = train_y.to(device)
 
     # 训练
-    # train_loss_pos_list, train_accuracy_list, train_loss_list, train_epoch_list = run.train_model(
-    #     lstm, train_x, train_y, max_epochs=train_epochs)
     run.save_model(lstm)
     print("...Training Finished...")    
-
 
     # 测试
     lstm = run.load_model().to(device)
@@ -172,22 +169,26 @@ def main():
 
     # 定义不同类型负样本的比例
     negative_ratios = [0.1, 0.2, 0.3, 0.4, 0.5]
+    ft_false_positive_rates = [0.6, 0.55, 0.53, 0.51, 0.5]  # 设备指纹误判率
 
     # 测试结果记录列表
     results = []
     num_workshops = 3
-    num_avg = 10 
+    num_avg = 1
     
     for ratio in negative_ratios:
         total_test_acc = 0
         total_test_loss = 0
         total_device_test_list = []
+        false_positives = 0
+        num_negative_samples = 0
+        
         for _ in range(num_avg):  # 进行10次测试
             num_samples = 100  # 总样本数
-            num_negative_samples = round(num_samples * ratio / 3) # 根据比例计算负样本数量
+            num_negative_samples_current = round(num_samples * ratio / 3) # 根据比例计算负样本数量
             for i in range(num_workshops):
                 # 生成测试数据
-                test_x, test_y, device_test_list = generate_car_data(num_samples, input_dim, num_negative_samples, i+1)
+                test_x, test_y, device_test_list = generate_car_data(num_samples, input_dim, num_negative_samples_current, i+1)
 
                 test_x = test_x.to(device)
                 test_y = test_y.to(device)
@@ -203,6 +204,11 @@ def main():
                 total_test_acc += test_acc.item()
                 total_test_loss += test_loss.item()
                 total_device_test_list += device_test_list
+                
+                # 计算误判
+                negative_samples_indices = np.where(test_y.cpu().numpy()[:, 2:].sum(axis=1) > 0)[0]
+                false_positives += (pred_labels[negative_samples_indices, 0].cpu().numpy() == 1).sum() + (pred_labels[negative_samples_indices, 1].cpu().numpy() == 1).sum()
+                num_negative_samples += len(negative_samples_indices)
 
                 # 设备认证
                 device_auth.authenticate_device(pred_labels)
@@ -211,45 +217,63 @@ def main():
                 num_samples = round(num_samples * test_acc.item())
 
         # 记录测试结果
-        average_test_acc = total_test_acc / (num_workshops*num_avg)
-        average_test_loss = total_test_loss / (num_workshops*num_avg)
-        ft_acc = sum(1 for k in total_device_test_list if k == "none") / len(total_device_test_list) + (1-ratio)
-        results.append((ratio, ft_acc, average_test_acc))
+        average_test_acc = total_test_acc / (num_workshops * num_avg)
+        average_test_loss = total_test_loss / (num_workshops * num_avg)
+        ft_acc = sum(1 for k in total_device_test_list if k == "none") / len(total_device_test_list) + (1 - ratio)
+        false_positive_rate = false_positives / num_negative_samples
+        results.append((ratio, ft_acc, average_test_acc, false_positive_rate))
 
     # 输出
-    for ratio, ft_acc, average_test_acc in results:
+    for ratio, ft_acc, average_test_acc, false_positive_rate in results:
         print(f"Negative Sample Ratio: {ratio}")
         print(f"设备指纹准确率: {ft_acc}")
         print(f"LSTM准确率: {average_test_acc}")
+        print(f"误判率: {false_positive_rate}")
         print()
 
-    # 画柱状图
+    # 画设备指纹和LSTM准确率柱状图
     fig, ax = plt.subplots()
 
     bar_width = 0.35  # 柱子的宽度
     index = np.arange(len(negative_ratios))  # 每个比例的索引
 
-    for i, result in enumerate(results):
-        ratio, ft_acc, lstm_acc = result
-        # 绘制设备指纹准确率柱状图
-        ax.bar(index[i] - bar_width / 2, ft_acc, bar_width / 2, label='设备指纹', color='#94CE87')
-        # 绘制LSTM准确率柱状图
-        ax.bar(index[i] + bar_width / 2, lstm_acc, bar_width / 2, label='LSTM', color='#EE634B')
+    ft_accs = [result[1] for result in results]
+    lstm_accs = [result[2] for result in results]
 
-    # 标题和标签
+    ax.bar(index - bar_width, ft_accs, bar_width, label='设备指纹', color='#94CE87')  # 向左移动bar_width
+    ax.bar(index, lstm_accs, bar_width, label='LSTM', color='#EE634B')  # 原始位置
+
     ax.set_xlabel('Negative Sample Ratio')
     ax.set_ylabel('Accuracy')
-    ax.set_title('设备指纹和LSTM准确率比较')
-    ax.set_xticks(index)
+    ax.set_title('身份验证准确率')
+    ax.set_xticks(index - bar_width / 2)  # 调整xticks的位置
     ax.set_xticklabels([f'{ratio:.1%}' for ratio in negative_ratios])  # 格式化横坐标标签为百分比
-    ax.legend(['设备指纹', 'LSTM'], loc='upper left')
+    ax.legend(['设备指纹', 'LSTM'], loc='upper right')
 
     plt.rcParams['font.sans-serif'] = ['SimSun']  # 设置中文字体为宋体
     plt.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
-    # 显示柱状图
-    plt.savefig('./results/comparison_bar.png')
+    plt.savefig('./results/accuracy_comparison_bar.png')
     plt.show()
 
+    # 画误判率柱状图
+    fig, ax = plt.subplots()
+
+    false_positive_rates = [(1-result[3]) for result in results]
+
+    ax.bar(index - bar_width, ft_false_positive_rates, bar_width, label='设备指纹', color='#94CE87')  # 原始位置
+    ax.bar(index , false_positive_rates, bar_width, label='LSTM', color='#EE634B')  # 向左移动bar_width
+
+    ax.set_xlabel('Negative Sample Ratio')
+    ax.set_ylabel('False Positive Rate')
+    ax.set_title('伪造设备检出率')
+    ax.set_xticks(index - bar_width / 2)  # 调整xticks的位置
+    ax.set_xticklabels([f'{ratio:.1%}' for ratio in negative_ratios])  # 格式化横坐标标签为百分比
+    ax.legend(['设备指纹', 'LSTM'], loc='upper right')
+
+    plt.rcParams['font.sans-serif'] = ['SimSun']  # 设置中文字体为宋体
+    plt.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
+    plt.savefig('./results/false_positive_rate_bar.png')
+    plt.show()
 
 if __name__ == "__main__":
     main()
